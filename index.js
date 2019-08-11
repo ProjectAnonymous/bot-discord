47fd10041359402d69f8594c739f3139f7bfd3ba
The-Guardians <kevenmichaud1@outlook.com>
2019-08-11 09:13:30 -0400

    Reset Discord AI

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-/*token request */
-const token = 'NTkyMzY0ODA1MTMwNTUxMjk2.XSJXpg.I-DcrweTzYOZDcNZS6MIrlt1lA8';
+const client = new Discord.Client();
 
-const PREFIX = '!';
-
-var version ="0.0.3";
-
-const usedCommandRecently = new Set();
-
-/*when bot is online*/
-bot.on('ready', () => {
-    console.log('The AI is now online!');
-    bot.user.setActivity('AI Engine');
-    bot.user.client.setActivity('');
-    bot.user.setAFK('300');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-/*token use */
-bot.login(token);
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
