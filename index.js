248a0bf4fa04b14ea91366fb98b30f1b283280a6
The-Guardians <kevenmichaud1@outlook.com>
2019-08-18 04:57:15 -0400

    Ajout de la mises èa jour commande fixé a 90% -> define member

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-const Discord = require('discord.js');
+const { Client, Collection } = require("discord.js");
 const { TOKEN, PREFIX } = require("./config");
-const client = new Discord.Client();
+const client = new Client({ disableEveryone: true });
+const fs =require("fs");
 
+client.PREFIX = PREFIX;
 
-client.on("message", msg => { 
-  if(msg.author.bot) return;
-  const args = msg.content.split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd === `${PREFIX}ping`) msg.channel.send("Pong!");
-  if (cmd === `${PREFIX}pong`) msg.channel.send("Ping!");
-  if (cmd === `${PREFIX}repeat`) {
-    msg.channel.send(args.join(" "));
-    msg.delete({ timeout: 3000 }).then(msg => console.log("Un Message à été Supprimé"));
-  }
-if (cmd === `${PREFIX}role`) {
-  const channel = client.channels.find(r => r.name === "logs");
-  const role = msg.guild.roles.find(r => r.name === args[0]);
-  if (!role) return msg.channel.send("Ce rôle n'existe pas !");
-  if (msg.member.roles.find(r => r.name === args[0])) {
-    msg.member.roles.remove(role);
-    channel.send(`J'ai supprimé le rôle ${role} à ${msg.author}.`);
-    msg.delete({ timeout: 3000 });
-  } else{
-    msg.member.roles.add(role);
-    channel.send(`J'ai ajouté le rôle ${role} à ${msg.author}.`);
-    msg.delete({ timeout: 3000 });
-  }
-}
-});
+client.commands = new Collection();
+client.commands.set("repeat", require("./commands/repeat.js"));
+client.commands.set("role", require("./commands/role.js"));
+client.commands.set("sinfo", require("./commands/sinfo.js"));
 
-client.on("guildMemberAdd", member => {
-  member.send("Salut à toi");
-  const channel = client.channels.find(r => r.name === "general-talk");
-  channel.send(`${member} just join the server!`);
-});
-
-
+client.on("ready", () => require("./events/ready.js")(client));
+client.on("message", msg => require("./events/message.js")(client, msg));
+client.on("guildMemberAdd", member = require("./events/guildMemberAdd.js")(client, member));
+//member not define  to be fix//
 client.login(TOKEN);
-
-client.on("ready", () => console.log("Nexus Is now Online!"));
 client.on("error", console.error);
 client.on("warn", console.warn);
-client.on("debug", console.log);
